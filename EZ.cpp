f13f803fa8783f75f1b020a8096af7d7778cfe75
carlolib <carlo_liberato@yahoo.com.ph>
2016-01-11 13:23:07 +0800

    First Practice...

---
+++ EZ.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+#define PI 3.14159
+
+
+void printNameCourse();
+float computeCircleArea(float radius);
+
+
+int main (){
+	
+	printNameCourse();
+	
+	/***************************************/
+	float area;
+	float radius;
+	
+	radius = 10;
+	
+	area = computeCircleArea(radius);
+	
+	printf("The area is %f\n", area);
+	
+	return EXIT_SUCCESS;
+	
+}
+
+void printNameCourse() {
+	puts("Carlo Liberato II");
+	puts("Hello LBYEC72 ");
+	puts ("5th Compile");
+}
+
+
+
+float computeCircleArea(float functionRadius){
+	return PI * functionRadius * functionRadius;
+}
